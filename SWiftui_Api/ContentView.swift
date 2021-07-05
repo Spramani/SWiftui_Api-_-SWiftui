71c444ad43df68e34d143fa1cf01b5a395a0402d
shubham P Ramani <shubhamramani903@gmail.com>
2021-07-05 09:44:02 +0530

    Initial Commit

---
+++ SWiftui_Api/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SWiftui_Api
+//
+//  Created by Adsum MAC 2 on 05/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SWiftui_Api/SWiftui_ApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWiftui_ApiApp.swift
+//  SWiftui_Api
+//
+//  Created by Adsum MAC 2 on 05/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SWiftui_ApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
